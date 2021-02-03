a5f09068c5e449aea0b7806c5cf0b8b48a1c62e1
wbaleco <wbaleco@hotmail.com>
2021-02-03 17:20:04 -0300

    Configuração inicial do projeto - EsLint + Prettier + Editorconfig

---
+++ Desktop/nextjs-clone-youtube/src/pages/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <h2>Clone YouTube com NextJS + Material-UI</h2>
+    </div>
+  );
+}
