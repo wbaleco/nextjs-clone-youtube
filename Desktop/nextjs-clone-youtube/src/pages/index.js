5668773a89a373f1117a46bda05e333ce16d73ad
Walter Baleco <Wbaleco@hotmail.com>
2021-02-03 22:22:20 -0300

    Configuração do Material-UI

--- Desktop/nextjs-clone-youtube/src/pages/index.js
+++ Desktop/nextjs-clone-youtube/src/pages/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <h2>Clone YouTube com NextJS + Material-UI</h2>
-    </div>
-  );
+    return (
+        <div>
+            clone Youtube
+            <Button variant="outlined" color="secondary">
+                Primary
+            </Button>
+        </div>
+    );
 }
